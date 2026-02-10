061a2e904ff254bd9ad1ee19232743122a7008a4
Vasiniyo <sdurdom2228@gmail.com>
2026-02-11 02:55:38 +0700

    feat: add cache for daily challenge

--- src/main/kotlin/render/BoardRenderService.kt
+++ src/main/kotlin/render/BoardRenderService.kt
@@ -4,19 +4,28 @@ import com.vasiniyo.app.domain.model.Board
 import java.awt.image.BufferedImage
 
 class BoardRenderService(private val piecePainter: PiecePainter) {
+    private val cache: MutableMap<Board, BufferedImage> = mutableMapOf()
+
     fun getDailyChallenge(board: Board, settings: BoardSettings): BufferedImage {
-        return ImageBoardBuilder(
-                board.turn,
-                settings.whiteTile,
-                settings.blackTile,
-                settings.lastTurnWhiteTile,
-                settings.lastTurnBlackTile,
-                settings.tileSize,
-                piecePainter
-            )
-            .withEmptyBoard(board.lastTurn)
-            .withNumbers()
-            .withPieces(board)
-            .build()
+        val cached = cache[board]
+        if (cached != null) {
+            return cached
+        }
+        val image =
+            ImageBoardBuilder(
+                    board.turn,
+                    settings.whiteTile,
+                    settings.blackTile,
+                    settings.lastTurnWhiteTile,
+                    settings.lastTurnBlackTile,
+                    settings.tileSize,
+                    piecePainter
+                )
+                .withEmptyBoard(board.lastTurn)
+                .withNumbers()
+                .withPieces(board)
+                .build()
+        cache[board] = image
+        return image
     }
 }
